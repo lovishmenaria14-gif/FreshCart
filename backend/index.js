3e69f9c75e7eff28ccf4e24d9e6ba6b957de22f0
Lovish Menaria <your-github-email@example.com>
2026-07-03 19:33:02 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -34,12 +34,17 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRouter)
+    origin: [
+        "http://localhost:5173",
+        "https://fresh-cart-steel-ten.vercel.app"
+    ],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 app.use("/api/user",userRouter)
 app.use("/api/shop",shopRouter)
 app.use("/api/item",itemRouter)
