996906c5e5cffa17dfdf15c848959a342d342bd9
Lovish Menaria <lovishmenaria14008@gmail.com>
2026-07-09 14:13:47 +0530

    added firebase

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -7,14 +7,38 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "FreshCart-food-delivery.firebaseapp.com",
-  projectId: "FreshCart-food-delivery",
-  storageBucket: "FreshCart-food-delivery.firebasestorage.app",
-  messagingSenderId: "693314883513",
-  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
+ authDomain: "freshcart-2bb3d.firebaseapp.com",
+  projectId: "freshcart-2bb3d",
+  storageBucket: "freshcart-2bb3d.firebasestorage.app",
+  messagingSenderId: "561312563221",
+  appId: "1:561312563221:web:e63295445d7684964f0e1c",
+  measurementId: "G-DHY6EK9PGP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app)
 export {app,auth}
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAxkQ7kj6-BocA_gzw9AU733ymo41mOlXk",
+//   authDomain: "freshcart-2bb3d.firebaseapp.com",
+//   projectId: "freshcart-2bb3d",
+//   storageBucket: "freshcart-2bb3d.firebasestorage.app",
+//   messagingSenderId: "561312563221",
+//   appId: "1:561312563221:web:e63295445d7684964f0e1c",
+//   measurementId: "G-DHY6EK9PGP"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
